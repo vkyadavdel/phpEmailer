6c6880517d437acbe26ee742a76e34dd4e0bf81a
vkyadavdel <76583450+vkyadavdel@users.noreply.github.com>
2023-05-02 11:54:31 +0530

    Add files via upload

---
+++ email.php
@@ -0,0 +1,32 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
+
+if(isset($_POST['send'])){
+    $name = htmlentities($_POST['name']);
+    $email = htmlentities($_POST['email']);
+    $subject = htmlentities($_POST['subject']);
+    $message = htmlentities($_POST['message']);
+
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'vkyadav.del@gmail.com';
+    $mail->Password = 'ycbsfvnbbijcsnne';
+    $mail->Port = 465;
+    $mail->SMTPSecure = 'ssl';
+    $mail->isHTML(true);
+    $mail->setFrom($email, $name);
+    $mail->addAddress('vkyadav.del@gmail.com');
+    $mail->Subject = ("$email ($subject)");
+    $mail->Body = $message;
+    $mail->send();
+
+    header("Location: ./response.html");
+}
+?>
